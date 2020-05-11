65a35fc57f079108e747088d05cd67f784ad1ead
lev <58232690+biggec@users.noreply.github.com>
2020-05-11 21:18:05 +0300

    /msg

--- client.py
+++ client.py
@@ -3,22 +3,12 @@ import json
 from threading import Thread
 
 
-ip = '194.87.238.112'
+ip = '127.0.0.1'
 client = goto_network.Client(ip, 9090)
 connection = client.connection
 name = str(input('Введите Имя: '))
-
-Commands = [
-    {"command": "I_AM",
-     "name": name}]
-
-connection.send(json.dumps(Commands[0]))
-
 con_things = 0
-
-Commands = [
-            {"command": "I_AM",
-             "name": name}]
+Commands = []
 
 
 def send_message():
@@ -27,15 +17,25 @@ def send_message():
         Commands.append({"command": "NEW_MESSAGE",
                          "text": message,
                          "name": name})
+        try:
+            if message.split(' ')[0] == '/msg':
+                Commands.append({"command": "NEW_MESSAGE_TO",
+                                 "text": message.split(' ', 2)[2],
+                                 "name": name,
+                                 "to": message.split(' ')[1]})
+                connection.send(json.dumps(Commands[3]))
+                Commands.pop(3)
+            elif message == 'end':
+                connection.send(json.dumps(Commands[0]))
+                connection.close()
+                break
 
-        if message == 'end':
-            connection.send(json.dumps(Commands[1]))
-            connection.close()
-            break
+            else:
+                connection.send(json.dumps(Commands[2]))
+                Commands.pop(2)
+        except:
+            pass
 
-        else:
-            connection.send(json.dumps(Commands[2]))
-        Commands.pop(2)
     return 0
 
 
@@ -49,11 +49,15 @@ def recv_message():
                 print(data)
             try:
                 if json.loads(data):
-                    con_things = json.loads(data)[1]
-                    print(con_things)
+                    con_things_ = json.loads(data)[1]
                     Commands.append({"command": "GOODBYE",
                                      "name": name,
                                      "con_things": con_things})
+                    Commands.append({"command": "I_AM",
+                                     "name": name,
+                                     "con_things": con_things_})
+                    print(con_things_)
+                    connection.send(json.dumps(Commands[1]))
             except:
                 pass
         except:
